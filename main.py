c27848ee1c216d57d41a8faebe1a809166c3648b
Michael Hicks <hicks.michael04@gmail.com>
2022-11-23 13:29:26 -0700

    create food class with turtle inheritance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from turtle import Turtle, Screen
-import time
+from turtle import Screen
 from snake import Snake
+from food import Food
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,7 @@ screen.title('My Snake Game')
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 # key presses
 screen.listen()
--- practice.py
+++ practice.py
@@ -1,2 +1,27 @@
-import code
-code.interact(local=dict(globals(), **locals()))
+# import code
+# code.interact(local=dict(globals(), **locals()))
+
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("Inhale, Exhale.")
+
+
+class Fish(Animal):
+
+    def __init__(self):
+        super().__init__()
+
+    def breathe(self):
+        super().breathe()
+        print('doing this under water')
+
+    def swim(self):
+        print('moving in water')
+
+
+nemo = Fish()
+nemo.breathe()
+print(nemo.num_eyes)
