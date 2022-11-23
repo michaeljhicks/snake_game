6b0eef559cbd871b1ce3160c4f37b9cb30ef552f
Michael Hicks <hicks.michael04@gmail.com>
2022-11-23 10:26:32 -0700

    make last two segments move atop first segment

--- main.py
+++ main.py
@@ -22,8 +22,15 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+
+        # for seg_num in range(start=2, stop=0, step=-1):
+        # segments[seg_num] = goto.xcor()
+        # segments[seg_num] = goto
 
 
 screen.exitonclick()
