56e50cc67eb5c2fe437629140dc5de5f76e4cb40
Michael Hicks <hicks.michael04@gmail.com>
2022-11-23 16:09:41 -0700

    create scoreboard class

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from snake import Snake
 from food import Food
 import time
 
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
---
+++ scoreboard.py
@@ -0,0 +1,8 @@
+from turtle import *
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
