cffd9ebbf0d918e98700efb47c3c3ad2e60c9c78
Michael Hicks <hicks.michael04@gmail.com>
2022-12-03 14:00:15 -0700

    add functionality to keep track of score

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 # key presses
 screen.listen()
@@ -29,6 +31,7 @@ while game_is_on:
     # Detech collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
         # for seg_num in range(start=2, stop=0, step=-1):
         # segments[seg_num] = goto.xcor()
--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = 'center'
+FONT = ('Courier', 24, 'normal')
 
 
 class Scoreboard(Turtle):
@@ -6,5 +8,17 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.write(f"Score: {self.score}", align="center",
-                   font=('Arial', 24, 'normal'))
+        self.color('white')
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT,
+                   font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
