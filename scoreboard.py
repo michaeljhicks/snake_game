4c130c8be91c6761ac22627e62558cf654d8df16
Michael Hicks <hicks.michael04@gmail.com>
2022-12-03 13:46:56 -0700

    create scoreboard class

--- scoreboard.py
+++ scoreboard.py
@@ -6,3 +6,5 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.write(f"Score: {self.score}", align="center",
+                   font='Arial, 24, normal')
