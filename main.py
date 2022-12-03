4fe665ae4ffd3fc3388a5aefe44bc1a8ccc38d5f
Michael Hicks <hicks.michael04@gmail.com>
2022-12-03 14:07:46 -0700

    delete TODOs in main

--- main.py
+++ main.py
@@ -44,27 +44,3 @@ while game_is_on:
 
 
 screen.exitonclick()
-
-# TODO: 1. Create a snake body
-# segment_1 = Turtle('square')
-# segment_1.color('white')
-# segment_2 = Turtle('square')
-# segment_2.color('white')
-# segment_2.goto(-20, 0)
-# segment_3 = Turtle('square')
-# segment_3.color('white')
-# segment_3.goto(-40, 0)
-
-# easier way to generate squares
-# these are tuples
-# TODO: 2.1. Move the snake
-
-
-# TODO: 2.2. while loop to move the snake
-
-# TODO: 3. Create Snake Food
-
-# TODO: 4. Detect collision with food
-# TODO: 5. Create a scoreboard
-# TODO: 6. Detect collision with wall
-# TODO: 7. Detect collision with tail
